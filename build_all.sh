f33eca4a6a72e6183564892a2223037aa93ab4cc
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-08-11 15:48:57 +0100

    Added more Dockerfiles

---
+++ build_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+cd ../py27-pyqt510
+docker build . -t py27-pyqt510:1.0
+
+cd py35-base-ubuntu-16.04
+docker build . -t py35-base-ubuntu-16.04:1.0
+
+cd ../py35-pyqt56
+docker build . -t py35-pyqt56:1.0
+
+cd ../py35-pyqt57
+docker build . -t py35-pyqt57:1.0
+
+cd ../py35-pyqt58
+docker build . -t py35-pyqt58:1.0
+
+cd ../py35-pyqt59
+docker build . -t py35-pyqt59:1.0
+
+cd ../py35-pyqt510
+docker build . -t py35-pyqt510:1.0
+
+cd ../py36-base-ubuntu-16.04
+docker build . -t py36-base-ubuntu-16.04:1.0
+
+cd ../py36-pyqt511
+docker build . -t py36-pyqt511:1.0
