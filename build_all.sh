6c2d9140322bc2139dec0baab11b3e3755d9d146
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-18 12:43:16 +0100

    Updated docker images to include missing dependencies and update PyQt versions

--- build_all.sh
+++ build_all.sh
@@ -1,31 +1,34 @@
 #!/bin/bash -x
 
 cd py27-pyqt510
-docker build . -t glueviz/py27-pyqt510:1.1
-
-cd ../py35-base-ubuntu-14.04
-docker build . -t glueviz/py35-base-ubuntu-14.04:1.1
-
-cd ../py35-pyqt56
-docker build . -t glueviz/py35-pyqt56:1.1
+docker build . -t glueviz/py27-pyqt510:1.2
 
 cd ../py35-base-ubuntu-16.04
-docker build . -t glueviz/py35-base-ubuntu-16.04:1.1
+docker build . -t glueviz/py35-base-ubuntu-16.04:1.2
 
 cd ../py35-pyqt57
-docker build . -t glueviz/py35-pyqt57:1.1
+docker build . -t glueviz/py35-pyqt57:1.2
 
 cd ../py35-pyqt58
-docker build . -t glueviz/py35-pyqt58:1.1
+docker build . -t glueviz/py35-pyqt58:1.2
 
 cd ../py35-pyqt59
-docker build . -t glueviz/py35-pyqt59:1.1
+docker build . -t glueviz/py35-pyqt59:1.2
 
 cd ../py35-pyqt510
-docker build . -t glueviz/py35-pyqt510:1.1
+docker build . -t glueviz/py35-pyqt510:1.2
 
 cd ../py36-base-ubuntu-18.04
-docker build . -t glueviz/py36-base-ubuntu-18.04:1.1
+docker build . -t glueviz/py36-base-ubuntu-18.04:1.2
 
 cd ../py36-pyqt511
-docker build . -t glueviz/py36-pyqt511:1.1
+docker build . -t glueviz/py36-pyqt511:1.2
+
+cd ../py36-pyqt512
+docker build . -t glueviz/py36-pyqt512:1.2
+
+cd ../py36-pyside511
+docker build . -t glueviz/py36-pyside511:1.2
+
+cd ../py36-pyside512
+docker build . -t glueviz/py36-pyside512:1.2
--- push_all.sh
+++ push_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
-docker push glueviz/py27-pyqt510:1.1
-docker push glueviz/py35-base-ubuntu-16.04:1.1
-docker push glueviz/py35-pyqt56:1.1
-docker push glueviz/py35-pyqt57:1.1
-docker push glueviz/py35-pyqt58:1.1
-docker push glueviz/py35-pyqt59:1.1
-docker push glueviz/py35-pyqt510:1.1
-docker push glueviz/py36-base-ubuntu-18.04:1.1
-docker push glueviz/py36-pyqt511:1.1
+docker push glueviz/py27-pyqt510:1.2
+docker push glueviz/py35-base-ubuntu-16.04:1.2
+docker push glueviz/py35-pyqt57:1.2
+docker push glueviz/py35-pyqt58:1.2
+docker push glueviz/py35-pyqt59:1.2
+docker push glueviz/py35-pyqt510:1.2
+docker push glueviz/py36-base-ubuntu-18.04:1.2
+docker push glueviz/py36-pyqt511:1.2
+docker push glueviz/py36-pyqt512:1.2
+docker push glueviz/py36-pyside511:1.2
+docker push glueviz/py36-pyside512:1.2
